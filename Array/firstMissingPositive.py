db3dbac63a130ee92e20931041418b761cedea63
yby582911664 <yby4301955@gmail.com>
2019-09-01 01:40:56 -0500

    41 didn't solve

---
+++ Array/firstMissingPositive.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(n):
+            while nums[i] > 0 and nums[i] <= n and nums[nums[i]-1] != nums[i]:
+                nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+        for i in range(n):
+            if nums[i] != i+1:
+                return i + 1
+        return n + 1    
+        
+        # C++ 可编译通过，但python超时
--- README.md
+++ README.md
@@ -31,3 +31,9 @@
 
 ------
 
+***2019/8/31***
+
+*41. First Missing Positive* (haven't solve with python)
+
+------
+
