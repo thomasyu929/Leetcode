80df28a48401d4662e9ab002577808af25f671ff
yby582911664 <yby4301955@gmail.com>
2019-10-26 23:44:02 -0500

    newfolder

--- Array/jumpGame.py
+++ Array/jumpGame.py
@@ -2,7 +2,7 @@ class Solution:
     def canJump(self, nums):
         i, j = 0, 0
         for i in range(len(nums)):
-            if (j >= len(nums)-1) or (i >j):
+            if (j >= len(nums)-1) or (i >j):    # 如果当前坐标reach到不了，break
                 break
             else:
                 j = max(i + nums[i],j)
--- Array/removeElement.py
+++ Array/removeElement.py
@@ -21,23 +21,8 @@ Your function should return length = 5, with the first five elements of nums con
 Note that the order of those five elements can be arbitrary.
 
 It doesn't matter what values are set beyond the returned length.
-Clarification:
-
-Confused why the returned value is an integer but your answer is an array?
-
-Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
-
-Internally you can think of this:
-
-// nums is passed in by reference. (i.e., without making a copy)
-int len = removeElement(nums, val);
-
-// any modification to nums in your function would be known by the caller.
-// using the length returned by your function, it prints the first len elements.
-for (int i = 0; i < len; i++) {
-    print(nums[i]);
-}
 '''
+
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         j = 0
